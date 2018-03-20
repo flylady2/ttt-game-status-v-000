9736ebb517016e6db36a31cf42845dbe082481db
Leslie Stevens <flylady@mail.utexas.edu>
2018-03-20 14:24:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,29 @@
+require 'pry'
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], # top row  index =  0
+  [3, 4, 5], # middle row
+  [6, 7, 8], # bottom row
+  [0, 3, 6], # left columns
+  [1, 4, 7], # middle columns
+  [2, 5, 8], # right columns
+  [0, 4, 8], # Ltop Rbottom diagonals
+  [6, 4, 2] # Rtop Lbottom diagonal
+]
+
+
+def won?(board)
+  WIN_COMBINATIONS.each do|win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    board.detect{position_1 == "X" && position_2 == "X" and position_3 == "X"}
+  end
+end
